d93a742f06c9278d199fd4a61ad0f1e8866ff48d
Huggenguggen <lee2000yh@gmail.com>
2022-06-13 09:45:21 +0800

    updates to automate a bit more

--- Scraper.py
+++ Scraper.py
@@ -3,22 +3,21 @@ import pandas as pd
 import re
 
 file = 'DemandAllocationRptUG_R2.pdf'
-output = open("test.txt", "a")
-data = tb.read_pdf(file, pages = '1')
+data = tb.read_pdf(file, pages = '1-3')
 
-print(data[0].iloc[:,2].dropna())
-
-newdata = pd.DataFrame()
-
-for (colName, colData) in data[0].iloc[:,[2,5,6]].iteritems():
-  #output.write("\n------------------------------------------------\n")
-  newdata[colName] = colData.dropna()
-
-output.write(newdata.to_string())
+#print(data[0].iloc[:,2].dropna())
 
 
+defaultFileName = "page"
+filenumber = 0
+for item in data:
+  print(filenumber)
+  output = open(defaultFileName + str(filenumber) + ".txt", "a")
+  newdata = pd.DataFrame()
+  for (colName, colData) in item.iloc[:,[2,5,6]].iteritems():
+    #output.write("\n------------------------------------------------\n")
+    newdata[colName] = colData.dropna()
+  output.write(newdata.to_string())
+  output.close()
+  filenumber += 1
 
-#table = data.to_string()
-#output.write(data[0])
-  
-output.close()
